e2fab25bea215a60844178ca02fbb941a68e7667
kimjh96 <kimjhs@kakao.com>
2022-12-20 00:43:05 +0900

    Initialized Project

---
+++ rollup.config.dev.js
@@ -0,0 +1,47 @@
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+import packageJson from './package.json';
+
+const outputs = [
+  {
+    file: packageJson.main,
+    format: 'cjs'
+  },
+  {
+    file: packageJson.module,
+    format: 'esm'
+  }
+];
+
+const extensions = [...DEFAULT_EXTENSIONS, '.ts', '.tsx'];
+
+export default outputs.map((output) => {
+  return {
+    input: 'src/index.ts',
+    output,
+    external: [/@babel\/runtime/],
+    plugins: [
+      peerDepsExternal(),
+      babel({
+        babelHelpers: 'runtime',
+        exclude: 'node_modules/**',
+        plugins: ['@babel/plugin-transform-runtime'],
+        extensions
+      }),
+      commonjs({
+        extensions
+      }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      resolve({
+        extensions
+      }),
+      terser()
+    ]
+  };
+});
